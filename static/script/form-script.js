599c42144df37dc84e936956f557ce9dc5af3fd4
Hitesh <aayush24sahu@gmail.com>
2023-08-13 02:07:47 +0530

    changing url

--- static/script/form-script.js
+++ static/script/form-script.js
@@ -1,5 +1,5 @@
 //----FORM SCRIPT START----
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzCxgiJKtTNxK1gFH8BxvF7eRs-sFZc_tc9LWwjJ8bRmuoWfZOE6EgwjhXAo_ikLk99/exec'
+const scriptURL = 'url goes here'
 const form = document.forms['submit-to-google-sheet']
 const msg = document.getElementById("msg")
 
